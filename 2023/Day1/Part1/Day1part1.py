c32dfd27dc6e1557b467da2de3bc8087d808b5fc
ZachLTech <zlopezzl2021@gmail.com>
2023-12-02 22:32:11 -0500

    modified:   2023/Day1/Part1/Day1part1.py 	new file:   2023/Day1/Part1/Day1part1SAMPLE.py

--- 2023/Day1/Part1/Day1part1.py
+++ 2023/Day1/Part1/Day1part1.py
@@ -1,11 +1,8 @@
+# Openning Input File
 Day1File = open("2023/Day1/Day1Input.txt", "r")
-
-# Test Data
-test = ["1abc2", "pqr3stu8vwx","a1b2c3d4e5f", "treb7uchet"]
-
+#Declaring
 lines = []
 finalnum = "0"
-
 # Where the magic happens (dw about my var and arr names... I'm really bad at naming things....)
 # Also there're some of unnecessary print statements that were just for finding bugs
 for line in Day1File:
@@ -17,20 +14,7 @@ for line in Day1File:
     last = nums[-1]
     numstogether = (first + last)
     finalnum = (int(finalnum) + int(numstogether))
-    #print(f"first: {first} last: {last} numstogether: {numstogether} finalnum: {finalnum}")
-
-    # Code to sort each string from smallest number to largest then calculate that I made on accident without realizing and lost me time lol
-    """
-    orderedfinalnums = []
-    maxnum = max(nums)
-    minnum = min(nums)
-    if nums.index(maxnum) > nums.index(minnum):
-        orderedfinalnums.append(minnum)
-        orderedfinalnums.append(maxnum)
-    else:
-        orderedfinalnums.append(maxnum)
-        orderedfinalnums.append(minnum)
-    print(orderedfinalnums)
-    """
 # Prints answer :yay:
 print(finalnum)
+# Almost forgot to close
+Day1File.close()
